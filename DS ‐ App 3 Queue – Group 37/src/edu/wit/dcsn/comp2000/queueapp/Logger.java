e6369de79f79ab06b21a87694024fe9bcf912a0d
Avery Loftin <loftina@wit.edu>
2017-10-19 17:31:24 -0400

    kjh

---
+++ DS ‐ App 3 Queue – Group 37/src/edu/wit/dcsn/comp2000/queueapp/Logger.java
@@ -0,0 +1,19 @@
+package edu.wit.dcsn.comp2000.queueapp;
+
+public class Logger
+{
+    public Logger()
+    {
+        //open log file or create new one
+    }
+
+    public void toLog()
+    {
+
+    }
+
+    public void saveLog()
+    {
+
+    }
+}
