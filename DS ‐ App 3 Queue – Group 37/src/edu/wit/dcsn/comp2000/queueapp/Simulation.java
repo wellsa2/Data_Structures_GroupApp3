eeacc24477150ba62d58a05f670cf367fdebd46b
cucuzzaj <cucuzzaj@10.200.172.40>
2017-10-24 14:40:58 -0400

    documentation & sim work

--- DS ‐ App 3 Queue – Group 37/src/edu/wit/dcsn/comp2000/queueapp/Simulation.java
+++ DS ‐ App 3 Queue – Group 37/src/edu/wit/dcsn/comp2000/queueapp/Simulation.java
@@ -1,10 +1,25 @@
 package edu.wit.dcsn.comp2000.queueapp;
 
+import java.io.FileNotFoundException;
+
 public class Simulation {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 
-	}
+		Configuration theConfiguration = new Configuration();
+		
+		int numberOfTicks = theConfiguration.getTicks();
+		
 
+}
+	
+	public static void buildTrains(Configuration config){
+		
+	   config.getTrains();
+		
+		
+	   
+	}
+	
 }
